c914fa7dbeb0cd71e494b00d97f0b3e369309315
Alexandria <walkerciara24@gmail.com>
2024-11-16 22:43:11 -0500

    create Footer javascript file.

---
+++ fashion-blog/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer = ({ links }) => (
+    <footer>
+        <nav aria-label="Footer Navigation" role="navigation">
+            <ul>
+                {links.map((link, index) => (
+                    <li key={index}>
+                        <a href={link.url}>{link.text}></a>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+        <p className="copyright">&copy; 2013 Valet Industries, Inc</p>
+    </footer>
+);
+
+export default Footer;
