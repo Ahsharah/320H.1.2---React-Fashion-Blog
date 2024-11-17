718a6b5e540e8a59a9083f12c77343d956522a31
Alexandria <walkerciara24@gmail.com>
2024-11-16 22:25:15 -0500

    fixing components directory and creating Header file.

---
+++ fashion-blog/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => (
+    <header>
+        <h1>Satre's List</h1>
+        <h2>Better-Dressed People</h2>
+    </header>
+);
+
+export default Header;
